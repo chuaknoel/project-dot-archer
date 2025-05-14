265cf29a2e9c02b94cedafdb1f9b508bbf45a75f
jy_choi <155281768+chlwjddbs@users.noreply.github.com>
2025-05-14 17:20:40 +0900

    refacor : Scene
    씬의 오브젝트 위치 정리

---
+++ Assets/Scripts/Common/GameManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public void Awake()
+    {
+        if(instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}
