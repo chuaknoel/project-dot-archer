2195bc6ac9568bdcb0e9f9a8399783b3fec5cc49
jy_choi <155281768+chlwjddbs@users.noreply.github.com>
2025-05-14 16:43:15 +0900

    feat : inventory
    shop 에서 구입한 아이템 불러오기.
    불러온 아이템을 인벤토리 UI에 표시

--- Assets/Scripts/Dungeon/DungeonManager.cs
+++ Assets/Scripts/Dungeon/DungeonManager.cs
@@ -39,7 +39,7 @@ public class DungeonManager : MonoBehaviour //방 이동, 전체 흐름 등 맵 전체 책임
 
     void Start()
     {
-        SetPlayerData();
+      
         // 맵 생성
         rooms = roomGenerator.GenerateDungeon();
         cameraController = Camera.main.GetComponent<CameraController>();
@@ -51,6 +51,7 @@ public class DungeonManager : MonoBehaviour //방 이동, 전체 흐름 등 맵 전체 책임
             currentRoom = startRoom;
             cameraController.SetCameraBounds(currentRoom.GetRoomBounds());
             navigator.MovePlayerToRoom(currentRoom, player.gameObject, Vector2Int.zero); // 초기엔 방향 없음
+            SetPlayerData();
         }
         else
         {
